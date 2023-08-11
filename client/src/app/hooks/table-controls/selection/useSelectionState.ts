5ee05ae154cf45d778e9b7e44d7818e209993813
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-08-11 14:21:07 -0400

    Update TODOs based on latest discussions
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/hooks/table-controls/selection/useSelectionState.ts
+++ client/src/app/hooks/table-controls/selection/useSelectionState.ts
@@ -1,22 +1,22 @@
-// TODO do we want useSelectionStateCommon or does that stuff belong in use/getDerivedSelectionState?
+// TODO see discussion on thread with Scott.
+//      We want to preserve selection when filter/pagination changes, as objects,
+//      and update the objects as possible when new data comes in.
+//      We do NOT want to persist selections in the URL.
+// TODO to avoid requiring the TItem type in useTableControlState/useTableControlUrlParams,
+//      maybe do actually keep only ids in state and handle the caching of selected objects we've seen in useSelectionDerivedState
+//      but still allow a `initialSelectedItemIds` to be passed?
+
+// TODO do we want useSelectionStateCommon or does that stuff belong in useSelectionDerivedState?
 
 // TODO isItemSelectable in derived state fn
 // TODO how to handle the useEffect to deselect unselectable items when present if we define that logic in derived state?
-//      maybe the derived state returns selectedItems with them filtered by selectable. memoize that?
 // TODO how do we handle letting the user select items across multiple pages when some items may be unloaded?
 //      -- in useSelectionDerivedState, keep a cache of all the selected items we've seen by id, and keep them up to date when new item data comes in via useEffect.
 //      -- return selectedItems by mapping ids to either the item from the current page if present, or an item from the cache if missing (preserve referential equality with same-page stuff in the table row logic -- maybe that'll still work if it just all comes from the cache? test it).
-//      -- this won't work if we're reloading the page and restoring from url params...
-// TODO how do we handle "select all"? probably we don't, right? or just select the current page?
-//      -- but we still want it for local?
+// TODO "select all" does not apply on server-driven tables, only "select page". how do we specify/detect which we need?
 
-// TODO restructure useSelectionState and useSelectionUrlParams like the other concerns in table hooks - rely on idProperty, etc
-// TODO nice to have... figure out how to make the URL param piece of it optional? give useUrlParams a disabled flag? maybe make that optional for all the concerns? play around with that...
-// TODO nice to have... make useUrlParams just omit the param from the URL if it is using the default -- does this prevent all the history.replace thrashing on first page mount?
-
-// TODO figure out where it is being used right now with useLocalTableControls and play with it there
-
-// TODO should we rename ActiveRowState to ActiveItemState? everything is driven from items not rows?
+// TODO UNRELATED nice to have... make useUrlParams just omit the param from the URL if it is using the default -- does this prevent all the history.replace thrashing on first page mount?
+// TODO UNRELATED should we rename ActiveRowState to ActiveItemState? everything is driven from items not rows?
 
 import * as React from "react";
 import { useUrlParams } from "../../useUrlParams";
